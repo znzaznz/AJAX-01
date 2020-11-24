c1da279b7c1d255da3a569ef8a872c1213102327
xiao <779740337@qq.com>
2020-11-24 15:41:58 +0800

    完成Promise重写AJAX

--- public/main.js
+++ public/main.js
@@ -1,23 +1,21 @@
-console.log("你好，我是main.js")
-
-button01.onclick = ()=>{
-    const request = new XMLHttpRequest();
-    request.open("get","/style.css")
-    request.onreadystatechange = ()=>{
-        if (request.readyState === 4){
-            if (request.status === 200){
-                console.log("style.css加载成功了");
-                console.log(request.response)
-                const style = document.createElement("style");
-                style.innerHTML = request.response
-                document.head.appendChild(style);
-            }else if (request.status === 404){
-                console.log("失败了")
-            }
-        }
-    }
-    request.send()
-}
+//添加一个css
+// button01.onclick = ()=>{
+//     const request = new XMLHttpRequest();
+//     request.open("get","/style.css")
+//     request.onreadystatechange = ()=>{
+//         if (request.readyState === 4){
+//             if (request.status === 200){
+//                 const style = document.createElement("style");
+//                 style.innerHTML = request.response
+//                 document.head.appendChild(style);
+//             }else if (request.status === 404){
+//                 console.log("失败了")
+//             }
+//         }
+//     }
+//     request.send()
+// }
+//添加一个js
 button02.onclick = ()=>{
     const request = new XMLHttpRequest();
     request.open("get","./2.js")
@@ -30,6 +28,7 @@ button02.onclick = ()=>{
     }
     request.send()
 }
+//添加一个html
 button03.onclick = ()=>{
     const request = new XMLHttpRequest();
     request.open("get","/3.html")
@@ -46,6 +45,7 @@ button03.onclick = ()=>{
     }
     request.send()
 }
+//添加一个xml
 button04.onclick = ()=>{
     const request = new XMLHttpRequest();
     request.open("get","/4.xml")
@@ -61,6 +61,7 @@ button04.onclick = ()=>{
     }
     request.send()
 }
+//添加一个JSON
 button05.onclick = ()=>{
     const request = new XMLHttpRequest();
     request.open("get","./5.json")
@@ -74,7 +75,7 @@ button05.onclick = ()=>{
     }
     request.send()
 }
-
+//添加一个单页
 let n = 1;
 button06.onclick = ()=>{
     const request = new XMLHttpRequest();
@@ -96,3 +97,30 @@ button06.onclick = ()=>{
     }
     request.send()
 }
+
+
+// 这里发现，不断的写AJAX实在太麻烦了，于是我考虑，我们用Promise封装一下
+ajax = (method="get",url,options)=>{
+    return new Promise(((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open(method,url)
+        request.onreadystatechange = ()=>{
+            if (request.readyState === 4){
+                if (request.status === 200){
+                    resolve.call(null,request.response)
+                }else {
+                    reject.call(null,request)
+                }
+            }
+        }
+        request.send(options)
+    }))
+}
+// 于是对昨天写的代码做一个重写
+button01.onclick = ajax("get","/style.css").then((e)=>{
+    const style = document.createElement("style")
+    style.innerHTML = e;
+    document.head.appendChild(style);
+},(e)=>{
+    console.log(e);
+})
